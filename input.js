075bcd1f68b3bcd8e2f6ec5f70aa52c69520f038
MilkJug <michael.sagan.crawford@gmail.com>
2024-06-17 10:07:10 -0400

    First exercise done

--- input.js
+++ input.js
@@ -3,13 +3,15 @@ let messages = document.querySelector(".messages");
 
 // CODE ALONG, PART 1 CONTINUED
 // 3. Declare a variable to store the input element
+let commentBox = document.querySelector(".comment-box");
 
 
 submitButton.addEventListener("click", function() {
     // 4. Declare a variable to store the value of the input
     // 5. Use the console to check a value is saved.
     //  - Type something and click the button to confirm it works!
-
+    let userInput = commentBox.value;
+    console.log(userInput);
 
     
 
